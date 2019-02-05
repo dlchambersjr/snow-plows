6baf142e8c45226ff931be5d1c933b93e72e0942
David Chambers <dlchambersjr@gmail.com>
2019-02-05 09:51:44 -0800

    stop at timebox requirement

--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,73 @@
 import React from 'react';
 
-const Form = props => {
-  return (
-    <div>
-      <h1>{props.data.plowID}</h1>
-      <h1>{props.data.plowType}</h1>
-      <h1>{props.data.location}</h1>
-    </div>
-  )
-}
 
-export default Form;
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props)
+
+    this.state = {
+      id: "empty",
+      type: "empty",
+      location: "empty",
+    };
+
+  }
+
+  handleChange = (event) => {
+    // let name = event.target.name;
+    // let value = event.target.value;
+    // console.log(name, value);
+    // this.setState({ [name]: value })
+
+
+    // console.log('event data', event.target);
+
+    // let id = event.target.id.value;
+    // let type = event.target.type.value;
+    // let location = event.target.location.value;
+
+    // this.setState({ id, type, location });
+
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // let id = event.target.id.value;
+    // let type = event.target.type.value;
+    // let location = event.target.location.value;
+
+    // console.log(id, type, location);
+
+    this.setState({
+      id: event.target.id.value,
+      type: event.target.type.value,
+      location: event.target.location.value
+    });
+
+    // this.setState({ id: id, type: type, location: location });
+
+
+    // let updateData = {
+    //   id: event.target.id.value,
+    //   type: event.target.type.value,
+    //   location: event.target.location.value,
+    // }
+    // console.log(updateData);
+
+    // this.props.onSubmit(updateData);
+    // onChange={this.handleChange}
+  }
+
+  render() {
+
+    return (
+      <form onSubmit={this.handleSubmit} >
+        <label>ID: <input value={this.props.id} name="id" size="5" /></label>
+        <label>TYPE:  <input value={this.props.type} name="type" size="35" /></label>
+        <label>LOCATION:  <input value={this.props.location} name="location" size="15" /></label>
+        <button>Update</button>
+      </form>
+    );
+  }
+}
